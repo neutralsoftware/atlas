264ba9dde5e42277fb2a533fb1a89f208ad008fa
Max Van den Eynde <maxvdec@gmail.com>
2026-06-24 19:28:59 +0200

    Created some dock abstraction for the editor

---
+++ editor/core/dockManager.cpp
@@ -0,0 +1,55 @@
+/*
+* dockManager.cpp
+* As part of the Atlas project
+* Created by Max Van den Eynde in 2026
+* --------------------------------------
+* Description: Dock management functions
+* Copyright (c) 2026 Max Van den Eynde
+*/
+
+#include <editor/application/dockManager.h>
+#include "DockManager.h"
+
+
+EditorDockManager::EditorDockManager(ads::CDockManager* dockManager)
+    : dockManager(dockManager) {
+}
+
+ads::DockWidgetArea EditorDockManager::toAdsArea(EditorDockArea area) const {
+    switch (area) {
+    case EditorDockArea::Left:
+        return ads::LeftDockWidgetArea;
+    case EditorDockArea::Right:
+        return ads::RightDockWidgetArea;
+    case EditorDockArea::Bottom:
+        return ads::BottomDockWidgetArea;
+    case EditorDockArea::Center:
+        return ads::CenterDockWidgetArea;
+    }
+
+    return ads::LeftDockWidgetArea;
+}
+
+ads::CDockWidget* EditorDockManager::addPanel(const EditorDockPanelDesc& desc) {
+    auto* dock = new ads::CDockWidget(desc.title);
+    dock->setWidget(desc.widget);
+
+    if (!desc.icon.isNull()) {
+        dock->setIcon(desc.icon);
+    }
+
+    if (desc.area == EditorDockArea::Center) {
+        centerArea = dockManager->setCentralWidget(dock);
+    } else if (centerArea) {
+        dockManager->addDockWidget(toAdsArea(desc.area), dock, centerArea);
+    } else {
+        dockManager->addDockWidget(toAdsArea(desc.area), dock);
+    }
+
+    panels.insert(desc.id, dock);
+    return dock;
+}
+
+ads::CDockWidget* EditorDockManager::panel(const QString& id) const {
+    return panels.value(id, nullptr);
+}
---
+++ editor/views/editor.cpp
@@ -0,0 +1,87 @@
+/*
+* editor.cpp
+* As part of the Atlas project
+* Created by Max Van den Eynde in 2026
+* --------------------------------------
+* Description: Editor view and window
+* Copyright (c) 2026 Max Van den Eynde
+*/
+
+#include <editor/views/editorWindow.h>
+
+#include <QMenuBar>
+#include <QStyle>
+
+#include "DockManager.h"
+#include "DockWidget.h"
+#include "editor/debug.h"
+
+static ads::CDockWidget* makeDock(const QString& title, QWidget* content) {
+    auto* dock = new ads::CDockWidget(title);
+    dock->setWidget(content);
+    return dock;
+}
+
+EditorWindow::EditorWindow(QWidget* parent)
+    : QMainWindow(parent) {
+    setupWindow();
+    setupMenus();
+    setupDocks();
+}
+
+void EditorWindow::setupWindow() {
+    setWindowTitle("Atlas Engine");
+    resize(1280, 720);
+    menuBar()->setNativeMenuBar(false);
+
+    ads::CDockManager::setConfigFlag(ads::CDockManager::OpaqueSplitterResize, true);
+    ads::CDockManager::setConfigFlag(ads::CDockManager::FocusHighlighting, true);
+    ads::CDockManager::setConfigFlag(ads::CDockManager::DisableStylesheet, true);
+
+    coreManager = new ads::CDockManager(this);
+    setCentralWidget(coreManager);
+    dockManager = new EditorDockManager(coreManager);
+}
+
+void EditorWindow::setupMenus() {
+    auto* fileMenu = menuBar()->addMenu("File");
+    fileMenu->addAction(style()->standardIcon(QStyle::SP_FileIcon), "New");
+    fileMenu->addAction(style()->standardIcon(QStyle::SP_DialogOpenButton), "Open");
+    fileMenu->addAction(style()->standardIcon(QStyle::SP_DialogSaveButton), "Save");
+    fileMenu->addSeparator();
+    fileMenu->addAction(style()->standardIcon(QStyle::SP_DialogCloseButton), "Quit", this, &QWidget::close);
+
+    auto* editMenu = menuBar()->addMenu("Edit");
+    editMenu->addAction(style()->standardIcon(QStyle::SP_ArrowBack), "Undo");
+    editMenu->addAction(style()->standardIcon(QStyle::SP_ArrowForward), "Redo");
+    editMenu->addSeparator();
+    editMenu->addAction("Preferences");
+
+    auto* viewMenu = menuBar()->addMenu("View");
+    viewMenu->addAction(style()->standardIcon(QStyle::SP_FileDialogDetailedView), "Reset Layout");
+
+    auto* windowMenu = menuBar()->addMenu("Window");
+    windowMenu->addAction(style()->standardIcon(QStyle::SP_TitleBarNormalButton), "Minimize");
+    windowMenu->addAction(style()->standardIcon(QStyle::SP_TitleBarMaxButton), "Zoom");
+
+    auto* helpMenu = menuBar()->addMenu("Help");
+    helpMenu->addAction(style()->standardIcon(QStyle::SP_MessageBoxQuestion), "About Atlas");
+}
+
+void EditorWindow::setupDocks() {
+    dockManager->addPanel({
+        .id = "debug1",
+        .title = "Debug Panel",
+        .widget = new DebugComponentsView(),
+        .area = EditorDockArea::Left,
+        .icon = style()->standardIcon(QStyle::SP_DirOpenIcon)
+    });
+
+    dockManager->addPanel({
+        .id = "debug2",
+        .title = "Debug Panel",
+        .widget = new DebugComponentsView(),
+        .area = EditorDockArea::Right,
+        .icon = style()->standardIcon(QStyle::SP_DirOpenIcon)
+    });
+}
