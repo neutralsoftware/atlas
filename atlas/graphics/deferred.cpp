b06fd280c64bcdc075b3baa985c45163d68e7db5
Max Van den Eynde <maxvdec@gmail.com>
2026-07-20 19:15:42 +0200

    Reversed IBL lighting technique

--- atlas/graphics/deferred.cpp
+++ atlas/graphics/deferred.cpp
@@ -972,8 +972,6 @@ void Window::deferredRendering(
         lightPipeline->bindTextureCubemap(
             "skybox", fallbackSkyboxTexture->textureID, boundTextures);
     }
-    lightPipeline->setUniformBool(
-        "useIBL", scene->skybox != nullptr && scene->skybox->cubemap.id != 0);
     boundTextures++;
 
     lightPipeline->setUniform1f(
